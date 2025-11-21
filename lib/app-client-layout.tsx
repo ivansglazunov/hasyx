6fc77ed3e35bc1685ad235cf71edecc2a6045f5b
Aruseli <llingveri@gmail.com>
2025-11-21 18:47:10 +0300

    Update version to 0.2.0-alpha.71 and add PWA support to AppClientLayout
    - Bumped the version in package.json to 0.2.0-alpha.71.
    - Enhanced AppClientLayout to include an optional PWA prop, allowing for better configuration of progressive web app features.
    
    Files modified:
    - package.json: Version update.
    - lib/app-client-layout.tsx: Added optional PWA support to the layout component.

--- lib/app-client-layout.tsx
+++ lib/app-client-layout.tsx
@@ -9,12 +9,13 @@ export function AppClientLayout(props: {
   schema: any;
   children: React.ReactNode;
   defaultTheme?: string;
+  pwa?: boolean;
 }) {
-  const { defaultLocale, schema, children, defaultTheme } = props;
+  const { defaultLocale, schema, children, defaultTheme, pwa = false } = props;
   const { locale } = useLocaleClient(defaultLocale);
   const messages = i18nMessages(locale);
   return (
-    <HasyxClientLayout defaultLocale={defaultLocale} defaultTheme={defaultTheme} schema={schema} messages={messages}>
+    <HasyxClientLayout defaultLocale={defaultLocale} defaultTheme={defaultTheme} schema={schema} messages={messages} pwa={pwa}>
       {children}
     </HasyxClientLayout>
   );
